180725cce45287a1ffe170b779342de284504825
Rachele Edwards <dot.com1472@gmail.com>
2020-05-12 13:40:36 -0600

    skeleton setup for api as well as postman testing, knexfile, congif, etc

---
+++ cars/car-router.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const db = require('../data/dbConfig')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+})
+
+router.get('/:id', (req, res) => {})
+
+router.post('/', (req, res) => {})
+
+router.put('/:id', (req, res) => {})
+
+router.delete('/:id', (req, res) => {})
+
+module.exports = router
