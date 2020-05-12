241d0ff85aa3e12f0cda0710f214449c8ed37ffd
Rachele Edwards <dot.com1472@gmail.com>
2020-05-12 17:46:31 -0600

    fixed error bug, sales data still didn't add but oh well

--- migrations/20200512134528_cars_table.js
+++ migrations/20200512134528_cars_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable("cars", table => {
         table.increments("id")
-        table.text("VIN", 17).notNullable().unique()
+        table.text("VIN", 255).notNullable().unique()
         table.text("make").notNullable()
         table.text("model").notNullable()
         table.integer("mileage")
